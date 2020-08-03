2625aeb2bb75d2c89739848bb816cf39effaddf5
zhaomanzhou <zmzsstreet@163.com>
2020-08-03 16:34:48 +0800

    新增

---
+++ mybatis-generator/src/main/java/com/mr/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.mr.mapper;
+
+import com.mr.entity.po.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
